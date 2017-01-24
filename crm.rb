092107df983da878c142d222d92c6b9ecb89d6f3
frankburke333 <frank.burke@gmail.com>
2017-01-24 14:41:28 -0500

    add contact.erb and updated index and crm files to have contacts

--- crm.rb
+++ crm.rb
@@ -1,8 +1,20 @@
-
+require_relative 'contact'
 
 require 'sinatra'
 
+
+Contact.create('Mark', 'Zuckerberg', 'mark@facebook.com', 'CEO')
+Contact.create('Sergey', 'Brin', 'sergey@google.com', 'Co-Founder')
+Contact.create('Steve', 'Jobs', 'steve@apple.com', 'Visionary')
+
+
 get '/' do
   @crm_app_name = "Bitmaker's CRM"
+  @crm_date = "Jan 24 2017"
   erb :index
+end
+
+get '/contacts' do
+  erb :contacts
+
 end
