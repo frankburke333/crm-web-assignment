044ba16c4c6f10feb1c0e31ff4ebce7ef8b48c38
frankburke333 <frank.burke@gmail.com>
2017-01-25 19:46:41 -0500

    completed second assignment

--- crm.rb
+++ crm.rb
@@ -2,10 +2,7 @@ require_relative 'contact'
 
 require 'sinatra'
 
-Contact.create('Frank', 'Burke', 'frank@bitmakerlabs.com', 'Developer')
-Contact.create('Walter', 'Bass', 'walter@bitmakerlabs.com', 'Developer')
-Contact.create('Donald', 'Trump', 'douche@bitmakerlabs.com', 'Developer')
-Contact.create('John', 'Maclean', 'die_hard@bitmakerlabs.com', 'Developer')
+
 
 get '/' do
   @crm_app_name = "Bitmaker's CRM"
